f862f17c035fd295c6b780d42bb9c1fa6454f930
Nikolay Ekaterinin <nikolai_e@rambler.ru>
2021-05-01 21:55:47 +0300

    add_default_password

---
+++ include/password.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef PASSWORD_H
+#define PASSWORD_H
+
+extern const char *Default_ssid;
+extern const char *Default_password;
+
+#endif
+
---
+++ src/password.cpp
@@ -0,0 +1,5 @@
+#include "password.h"
+
+// Your WIFI/NTP data: 
+const char *Default_ssid = "YourWiFi";
+const char *Default_password = "YourPassword";
